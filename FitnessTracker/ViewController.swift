1e4572ed2faceb4943f6ffe4ae97233c5bf6f9a4
Pollux <pollux@polluxs-mac.local>
2016-07-06 13:50:02 +0200

    Setup the default image and the image picker

--- FitnessTracker/ViewController.swift
+++ FitnessTracker/ViewController.swift
@@ -8,12 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate   {
 
     // MARK: Properties
     
     @IBOutlet weak var deviceNameLabel: UILabel!
     @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var photoImageView: UIImageView!
     
     
     override func viewDidLoad() {
@@ -33,7 +34,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     func textFieldShouldReturn(textField: UITextField) -> Bool {
         
-        // Hide the keyboard
+        // Hide the keyboard.
         
         textField.resignFirstResponder()
         
@@ -44,8 +45,53 @@ class ViewController: UIViewController, UITextFieldDelegate {
         deviceNameLabel.text = textField.text
     }
     
+    // MARK: UIImagePickerControllerDelegate
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        
+        // Dismiss the picker if the user canceled.
+        
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        
+        // The info dictionary contains multiple representations of the image, and this uses the original.
+        
+        let selectedImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        // Set photoImageView to display the selected image.
+        
+        photoImageView.image = selectedImage
+        
+        // Dismiss the picker.
+        
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
     
     // MARK: Actions
+    
+    @IBAction func selectImage(sender: UITapGestureRecognizer) {
+        
+        // Hide the keyboard.
+        
+        nameTextField.resignFirstResponder()
+        
+        // UIImagePickerController is a view controller that lets a user pick media from their photo library or camera
+        
+        let imagePickerController = UIImagePickerController()
+        
+        // Set the controller to only let a user choose from library and not from camera for now.
+        
+        imagePickerController.sourceType = .PhotoLibrary
+        
+        // Make sureViewController is notified when the user picks the image.
+        
+        imagePickerController.delegate = self
+        
+        presentViewController(imagePickerController, animated: true, completion: nil)
+    }
     
     @IBAction func setDefaultLAbelText(sender: UIButton) {
         deviceNameLabel.text = "Default text"
