a1c49af74aae7661265c2f72069caec73737f969
Pollux <pollux@polluxs-mac.local>
2016-07-06 15:07:22 +0200

    Defined data model for fitness device

---
+++ FitnessTracker/Device.swift
@@ -0,0 +1,38 @@
+//
+//  Device.swift
+//  FitnessTracker
+//
+//  Created by Pollux on 06/07/16.
+//  Copyright © 2016 Pollux. All rights reserved.
+//
+
+import UIKit
+
+    class Device {
+    
+        // MARK: Properties
+        
+        var name: String
+        var weight: Int
+        var seat: Int?
+        var photo: UIImage
+        
+        // MARK: Initialization
+        
+        init?(name: String, weight: Int, seat: Int?, photo: UIImage) {
+            
+            // Initialize stored properties.
+            
+            self.name = name
+            self.weight = weight
+            self.seat = seat
+            self.photo = photo
+            
+            // Initialization should fail if there is no name or the weight is negative.
+            
+            if name.isEmpty || weight < 0 {
+                return nil
+            }
+        }
+       
+    }
